7d6a89bbd2e15b38943f7cb79d5e1311e70cc949
Ololkao <h24076087@gs.ncku.edu.tw>
2021-10-21 14:59:26 +0800

    fill in the last column for missForest imputation

--- make_upload.py
+++ make_upload.py
@@ -8,7 +8,7 @@ def make_upload(imputer, model=LGBMRegressor(random_state=42), scaler=None, stat
     tp_mean = 0.0
     for i in range(1,8,1):
         if imputer is not None:
-            np_train, np_answer = np.array(globals()[f'train{i}']), np.array(globals()[f'answer{i}'])
+            np_train, np_answer = np.array(eval(f'r.train{i}')), np.array(eval(f'r.answer{i}'))
             X_train, X_valid, y_train, y_valid = train_test_split(np_train, np_answer, test_size=test_size, random_state=seed)
             if scaler is True:
                 X_train = scaler.fit_transform(X_train)
@@ -19,7 +19,7 @@ def make_upload(imputer, model=LGBMRegressor(random_state=42), scaler=None, stat
                 ft_valid = imputer.fit_transform(X_valid)
             else: ft_valid = imputer.transform(X_valid)
         else:
-            np_train, np_answer = np.array(eval(f'r.temp_train{i}')), np.array(globals()[f'answer{i}'])
+            np_train, np_answer = np.array(eval(f'r.temp_train{i}')), np.array(eval(f'r.answer{i}'))
             ft_train, ft_valid, y_train, y_valid = train_test_split(np_train, np_answer, test_size=test_size, random_state=seed)
 
         model.fit(ft_train, y_train)
@@ -30,7 +30,7 @@ def make_upload(imputer, model=LGBMRegressor(random_state=42), scaler=None, stat
 
         if status is True:
             if imputer is not None:
-                X_test = np.array(globals()[f'test{i}'])
+                X_test = np.array(eval(f'r.test{i}'))
                 if scaler is True: scaler.fit_transform(X_test)
                 if "fancyimpute" in imputer.__module__:
                     ft_test = imputer.fit_transform(X_test)
@@ -45,6 +45,6 @@ def make_upload(imputer, model=LGBMRegressor(random_state=42), scaler=None, stat
             if imputer is not None:
                 upload.to_csv(f'upload/{str(imputer)[:3]}_{str(model)[:4]}_upload{i}.csv', header=None, index=None, sep=',', mode='w')
             else:
-                upload.to_csv(f'upload/Forest_{str(model)[:4]}_upload{i}.csv', header=None, index=None, sep=',', mode='w')
+                upload.to_csv(f'upload/Forest_full_{str(model)[:4]}_upload{i}.csv', header=None, index=None, sep=',', mode='w')
         print()
     print("TP_mean in valid set = {}".format(tp_mean))
